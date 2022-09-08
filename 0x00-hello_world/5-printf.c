7c9ede3ce006d62381ebd3983fbc016ab92dcfa6
Abdulmelik619 <abdulmelikambaw619@gmail.com>
2022-09-08 06:56:30 -0700

    l

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
 /**
-* abd
+* main - print the string in the put function
 *
-* des
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle
+* Return: 0
 */
 int main(void)
 {
