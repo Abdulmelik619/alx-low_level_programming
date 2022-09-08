cdb89988617275188fddf7fc1e5226c40879e7c6
Abdulmelik619 <abdulmelikambaw619@gmail.com>
2022-09-08 06:33:58 -0700

    d

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,25 @@
 #include <stdio.h>                                                                                                                      
-int main(void)
-{
-puts ("\"Programming is like building a multilingual puzzle");
-return (0);
+
+                                                                                                                                        
+
+/**                                                                                                                                     
+ *
+ * * main - print the string in the put function                                                                                           
+ *
+ * *                                                                                                                                       
+ *
+ * * Description: using the main function                                                                                                  
+ *
+ * * this program prints "Programming is like building a multilingual puzzle                                                               
+ *
+ * * Return: 0                                                                                                                             
+ *
+ * */                                                                                                                                      
+
+int main(void)                                                                                                                          
+
+	                                                                                                                                        
+
+{                                                                                                                                       
+
+	puts ("\"Programming is like building a multilingual puzzle");                 ZZZ
