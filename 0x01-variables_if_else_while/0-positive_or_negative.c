df38b173f8533285baa1f89b96e9beeeb11ae00e
Abdulmelik619 <abdulmelikambaw619@gmail.com>
2022-09-09 04:23:22 -0700

    f

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -20,12 +20,19 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	if (n==0)	
-		printf("%i is zero\n",n);
+	if (n==0)
+	{	
+	printf("%i is zero\n",n);
+	}
 	else if (n < 0)
-		printf("%i is negative\n",n);
+	{
+	printf("%i is negative\n",n);
+	}
 	else 
-		printf("%i is positive\n",n);
+	{
+	printf("%i is positive\n",n);
+	}
+
 	return (0);
 }
 
