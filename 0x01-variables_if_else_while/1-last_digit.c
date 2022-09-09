3e822619516eebf5e049470b6258658818898feb
Abdulmelik619 <abdulmelikambaw619@gmail.com>
2022-09-09 04:41:02 -0700

    d

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,37 +1,32 @@
-#include <stdlib.h>                                                                                                                     
+#include <stdlib.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-#include <stdlib.h>                                                                                                                     
-
-#include <stdio.h>                                                                                                                      
-
-                                                                                                                                        
-
-/**                                                                                                                                     
+/**
+ * main -andb
  *
- *  * main -andb                                                                                                                           
- *
- *   *                                                                                                                                      
- *
- *    * Return = dsjhfvjhjhjv                                                                                                                
- *
- *     */                                                                                                                                     
+ * Return = dsjhfvjhjhjv
+ */
+int main(void)
 
-int main(void)                                                                                                                          
+{
+int n;
 
-	                                                                                                                                        
+srand(time(0));
+n = rand() - RAND_MAX / 2;
 
-{                                                                                                                                       
+if (n % 10 > 5)
+{
+printf("Last digit of %i is %i and is greater than 5\n",n,n%10);
+}
+else if (n % 10 == 0)
+{
+printf("Last digit of %i is %i and is 0\n",n,n%10);
+}
+else 
+{
+printf("Last digit of %i is %i and is lesss than 6 and not 0\n",n,n%10);
+}
 
-	int n;                                                                                                                                  
-
-	                                                                                                                                        
-
-	srand(time(0));                                                                                                                         
-
-	n = rand() - RAND_MAX / 2;                                                                                                              
-
-	                                                                                                                                        
-
-	if (n % 10 > 5)                                                                                                                         
-
-	{                    
+return (0);
+}
