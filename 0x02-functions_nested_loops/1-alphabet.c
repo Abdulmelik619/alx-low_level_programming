62994d6c19df2451bae4f773e600ebd02ae54a84
Abdulmelik619 <abdulmelikambaw619@gmail.com>
2022-09-13 03:36:04 -0700

    s

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main: hfhgv
+ *
+ * Description: bcvkncvkbvb ncbb j  
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+char a;
+for(a='a',a <='z',a++)
+{
+_putchar(a);
+}
+_putchar();
+}
