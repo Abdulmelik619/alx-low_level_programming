7b1427d1234c55e7d4ce694e7d0d7dab0fec7407
Abdulmelik619 <abdulmelikambaw619@gmail.com>
2022-09-15 04:56:36 -0700

    d

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,19 +1,17 @@
 #include "main.h"
 
 /**
- * _isdigit - hjhcbhcbjzxbk cb k xc
- * @c: bdcjhbcjshbz
- * Return: 1 or 0
- */
+*_isdigit - hjhcbhcbjzxbk cb k xc
+*@c: bdcjhbcjshbz
+*Return: 1 if it is, 0 otherwise
+*/
 
 int _isdigit(int c)
 {
-if (c<=57 && c>=48)
-{
-	return (1);
-}
-else
-{
+	if ((c<=57) && (c>=48))
+	{
+		return (1);
+	}
+
 	return (0);
-}
 }
