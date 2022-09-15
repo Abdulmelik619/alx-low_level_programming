937676249e59557819b44751e14a08962b2fe9e7
Abdulmelik619 <abdulmelikambaw619@gmail.com>
2022-09-15 04:38:23 -0700

    d

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - bhsvbkxbc
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+int a;
+a=0;
+
+while(a<10)
+{
+if(a==2 || a==4)
+	continue;
+else
+	_putchar(a);	
+}
+
+_putchar('\n');
+}
+
