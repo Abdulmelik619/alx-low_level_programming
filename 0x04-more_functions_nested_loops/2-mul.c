d6223fa26914d887620c45858d77275f10b0c2b8
Abdulmelik619 <abdulmelikambaw619@gmail.com>
2022-09-15 04:11:20 -0700

    f

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include  "main.h"
+
+/**
+ * mul - hjsavjhcds
+ * @a: fhgdrkbchvnbb 
+ * @b: kfhgiudhjnkhfbdgjkhbldgjkbxf
+ * Return: inteegeeerr
+ */
+
+int mul(int a, int b)
+{
+int c;
+c= a*b;
+return (c);
+}
