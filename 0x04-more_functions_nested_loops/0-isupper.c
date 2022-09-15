772412409d6a9a8d5dda473b05d725411992fd64
Abdulmelik619 <abdulmelikambaw619@gmail.com>
2022-09-15 04:51:14 -0700

    d

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * _isupper - return 1 if c i uppercase
- * @c: the character
- * Return: 1 whehter its 0 otherwise
- */
+*_isupper - return 1 if c i uppercase
+*@c: the character
+*Return: 1 whehter its 0 otherwise
+*/
 
 int _isupper(int c)
 {
