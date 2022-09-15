6e4a581e8499844a7796a39a36358c1ede9c14a7
Abdulmelik619 <abdulmelikambaw619@gmail.com>
2022-09-15 04:26:30 -0700

    d

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - ohfksbcxxcvckb
+ *Return: void
+ */
+
+void print_numbers(void)
+{
+int i=0;
+while(i<10)
+{
+_putchar(i);
+i=i+1;
+}
+_putchar('\n');
+}
