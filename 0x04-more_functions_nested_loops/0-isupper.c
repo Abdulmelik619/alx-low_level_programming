8de11da09b23b9e4f05daf8235b55eb2daf79194
Abdulmelik619 <abdulmelikambaw619@gmail.com>
2022-09-15 03:49:09 -0700

    d

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - return 1 if c i uppercase
+ * @c: the character
+ * Return: 0 orr 1
+ */
+
+int _isupper(int c)
+{
+if (c >= 65 && c>=90)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
